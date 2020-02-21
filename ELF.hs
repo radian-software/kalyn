966af3a540c34e77511a91b52089060488fb5e56
Radon Rosborough <radon.neon@gmail.com>
2020-02-20 21:07:15 -0800

    Conform to ELF spec correctly
    I got played by 64-bit ELF being different but not documented...

--- ELF.hs
+++ ELF.hs
@@ -10,12 +10,7 @@ import           Data.Word
 -- https://refspecs.linuxfoundation.org/elf/elf.pdf
 
 fixedPoint :: Eq a => a -> (a -> a) -> a
-fixedPoint x f = if x == f x then x else fixedPoint (f x) f
-
--- Virtual addresses near 0 are reserved, so we need to map the code
--- at a higher virtual address than its byte index in the file.
-virtualAddressOffset :: Word32
-virtualAddressOffset = 0x10000
+fixedPoint x f = let fx = f x in if x == fx then x else fixedPoint fx f
 
 -- see page 20
 elfIdent :: B.ByteString
@@ -32,15 +27,15 @@ elfIdent =
 elfHeader :: Word16 -> Word16 -> B.ByteString
 elfHeader elfHeaderLength programHeaderLength =
   let totalLength = elfHeaderLength + programHeaderLength
-      entryPoint  = fromIntegral totalLength + virtualAddressOffset
   in  toLazyByteString
         $  lazyByteString elfIdent
         <> word16LE 3 -- file type, relocatable executable (called "shared object file")
-        <> word16LE 3 -- architecture, Intel
+                    -- see https://opensource.apple.com/source/dtrace/dtrace-90/sys/elf.h
+        <> word16LE 62 -- architecture, x86_64
         <> word32LE 1 -- object file version
-        <> word32LE entryPoint -- entry point in virtual memory
-        <> word32LE (fromIntegral elfHeaderLength) -- program header offset
-        <> word32LE 0 -- section header offset, unused
+        <> word64LE (fromIntegral totalLength) -- entry point in virtual memory
+        <> word64LE (fromIntegral elfHeaderLength) -- program header offset
+        <> word64LE 0 -- section header offset, unused
         <> word32LE 0 -- processor-specific flags, none needed
         <> word16LE elfHeaderLength -- ELF header size
         <> word16LE programHeaderLength -- program header entry length
@@ -50,19 +45,18 @@ elfHeader elfHeaderLength programHeaderLength =
         <> word16LE 0 -- index of string table in section header, unused
 
 -- see page 40
-programHeader :: Word16 -> Word16 -> Word32 -> B.ByteString
+programHeader :: Word16 -> Word16 -> Word64 -> B.ByteString
 programHeader elfHeaderLength programHeaderLength imageSize =
   let totalLength = fromIntegral $ elfHeaderLength + programHeaderLength
-      va          = totalLength + virtualAddressOffset
   in  toLazyByteString
         $  word32LE 1 -- segment type, loadable code/data
-        <> word32LE totalLength -- offset from beginning of file
-        <> word32LE va -- virtual address at which to map code/data
-        <> word32LE 0 -- physical address at which to map, unused
-        <> word32LE imageSize -- number of bytes listed in file image
-        <> word32LE imageSize -- number of bytes to reserve in memory
         <> word32LE 0x7 -- permissions, allow all (see page 73)
-        <> word32LE 0 -- alignment, none required
+        <> word64LE totalLength -- offset from beginning of file
+        <> word64LE totalLength -- virtual address at which to map code/data
+        <> word64LE 0 -- physical address at which to map, unused
+        <> word64LE imageSize -- number of bytes listed in file image
+        <> word64LE imageSize -- number of bytes to reserve in memory
+        <> word64LE 0 -- alignment, none required
 
 -- see page 15
 elfData :: B.ByteString -> B.ByteString
