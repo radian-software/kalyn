f6ead57ed91d18d44310500948aa075637cf6eef
Radon Rosborough <radon.neon@gmail.com>
2020-02-26 12:55:56 -0800

    Add Emacs major mode

---
+++ kalyn-mode.el
@@ -0,0 +1,60 @@
+;;; kalyn-mode.el --- Major mode for Kalyn -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Created: 25 Feb 2020
+;; Keywords: languages
+;; Package-Requires: ((emacs "27"))
+;; Version: 0
+
+;;; Commentary:
+
+;; Major mode for editing Kalyn source code.
+
+;;; Code:
+
+(defvar kalyn-declaration-builtins
+  '("alias" "class" "data" "defn" "def" "derive" "instance" "public" "with"))
+
+(defvar kalyn-special-form-builtins
+  '("case" "lambda" "let"))
+
+(defun kalyn-builtins ()
+  (append kalyn-declaration-builtins kalyn-special-form-builtins))
+
+(defun kalyn-font-lock-keywords ()
+  (list (list (mapconcat
+               #'regexp-quote
+               (kalyn-builtins)
+               "\\|")
+              (list 0 'font-lock-keyword-face))
+        (list "\\_<[A-Z].*?\\_>"
+              (list 0 'font-lock-type-face))))
+
+(defun kalyn-indent-function (indent-point state)
+  (cl-block nil
+    ;; Go to beginning of innermost containing list. If not inside a
+    ;; list, indent to left margin.
+    (unless (nth 1 state)
+      (cl-return 0))
+    (goto-char (nth 1 state))
+    (+ 2 (current-column))))
+
+(define-derived-mode kalyn-mode prog-mode "Kalyn"
+  "Major mode for editing Kalyn code."
+  (setq-local font-lock-defaults
+              (list #'kalyn-font-lock-keywords))
+  (setq-local indent-line-function #'lisp-indent-line)
+  (setq-local lisp-indent-function #'kalyn-indent-function))
+
+(add-to-list 'auto-mode-alist '("\\.kalyn\\'" . kalyn-mode))
+
+(provide 'kalyn-mode)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; outline-regexp: ";;;;* "
+;; End:
+
+;;; kalyn-mode.el ends here
