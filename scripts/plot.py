b07eeb39dab92ec6703bc420b37a76d808677bf6
Radon Rosborough <radon.neon@gmail.com>
2020-05-20 10:09:33 -0600

    Generate graphs

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,6 +4,10 @@ import datetime
 import os
 import pathlib
 
+from matplotlib.dates import DateFormatter, date2num
+from matplotlib.ticker import FixedLocator
+import matplotlib.pyplot as plt
+
 os.chdir(pathlib.Path(__file__).parent)
 
 points = []
@@ -48,4 +52,63 @@ with open("report.log") as f:
             }
         )
 
-print(points)
+
+def combine(k1, k2, k3):
+    for point in points:
+        point[k3] = point[k1] + point[k2]
+
+
+def accumulate(key):
+    total = 0
+    for point in points:
+        total += point[key]
+        point[key + "_total"] = total
+
+
+combine("haskell_loc_added", "kalyn_loc_added", "loc_added")
+combine("haskell_loc_removed", "kalyn_loc_removed", "loc_removed")
+
+
+accumulate("loc_added")
+accumulate("loc_removed")
+
+
+months = [datetime.date(year=2020, month=n, day=1) for n in range(3, 6)]
+midmonths = [datetime.date(year=2020, month=n, day=16) for n in range(3, 6)]
+
+
+def plot(series, title, png):
+    # https://matplotlib.org/3.1.1/gallery/text_labels_and_annotations/date.html
+    fig, ax = plt.subplots(figsize=(24, 18))
+    t = [p["timestamp"] for p in points]
+    for key, label, color in series:
+        y = [p[key] for p in points]
+        (line,) = ax.plot(t, y, label=label, linewidth=10)
+        if color:
+            line.set_color(color)
+    ax.tick_params(labelsize=40)
+    ax.xaxis.set_major_formatter(DateFormatter("%b %-d"))
+    ax.xaxis.set_minor_locator(FixedLocator(list(map(date2num, midmonths))))
+    plt.xticks(months)
+    plt.rc("font", size=40)
+    plt.legend()
+    plt.title(title)
+    plt.grid(True, "both")
+    plt.savefig(png)
+
+
+plot(
+    [("haskell_loc", "Haskell", "orange"), ("kalyn_loc", "Kalyn", "purple")],
+    "Total lines of code",
+    "loc.png",
+)
+plot(
+    [("haskell_files", "Haskell", "orange"), ("kalyn_files", "Kalyn", "purple")],
+    "Number of modules",
+    "files.png",
+)
+plot(
+    [("loc_added_total", "Added", "green"), ("loc_removed_total", "Removed", "red")],
+    "Added and removed lines of code",
+    "codefreq.png",
+)
